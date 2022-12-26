b8ed16aeafcc33870649d8c3f599fc9986fccdd5
polluque <polluque26081997@gmail.com>
2022-12-26 11:13:37 -0500

    keep_notes_paulluque_v1.1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trabajo_final_keep_note/style/app_style.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,6 +11,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('FireNotes'),
+        centerTitle: true,
+        backgroundColor: AppStyle.mainColor        
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'tus notas recientes', 
+            style: GoogleFonts.roboto(color: Colors.white, fontWeight: FontWeight.bold, 
+            fontSize: 22,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

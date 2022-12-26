da548dd87c284087f334aaaf61d1dbf09e35fbc8
polluque <polluque26081997@gmail.com>
2022-12-26 13:37:26 -0500

    keep_notes_paulluque_v1.2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:trabajo_final_keep_note/style/app_style.dart';
+import 'package:trabajo_final_keep_note/widgets/note_card.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -19,17 +21,43 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: AppStyle.mainColor        
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'tus notas recientes', 
-            style: GoogleFonts.roboto(color: Colors.white, fontWeight: FontWeight.bold, 
-            fontSize: 22,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'tus notas recientes paul', 
+              style: GoogleFonts.roboto(color: Colors.white, fontWeight: FontWeight.bold, 
+              fontSize: 22,
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 20.0,
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('notes').snapshots(),
+              builder:(context, AsyncSnapshot snapshot) {
+                  // validando la conexion a firebase 
+                  if(snapshot.connectionState == ConnectionState.waiting){
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData){
+                    return GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                        children: 
+                        snapshot.data!.map((note) => noteCard(() {}, note)),
+                        );
+                  }
+                return Text('no exiten notas', style: GoogleFonts.nunito(color: Colors.white),);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
